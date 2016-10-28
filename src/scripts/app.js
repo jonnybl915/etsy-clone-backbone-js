1b36987940f1452fbaf7861b237fd215deec98c6
jonathandavidblack <jonnybl915@gmail.com>
2016-10-28 10:31:21 -0400

    added model and collection skelletons

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,24 @@
 const Backbone = require('Backbone');
 
+//setting up a model
+const EtsyModel = Backbone.Model.extend({
+	url: "" //we'll probably add this back in later with ETSY info
+})
+
+const EtsyCollection = Backbone.Collection.extend({
+	url: "" //this will also be added later with ETSY info
+})
+
 var AppRouter = Backbone.Router.extend({
 	routes: {
 		"": "showHomePage",
 	},
 
 	showHomePage: function(){
-		document.querySelector('#app-container').innerHTML = `<h1>START HER UP<h1>`;
+		document.querySelector('#app-container').innerHTML = `<h4>START HER UP<h4>`;
+
+		//this will be where we instantiate the model and collection to get the data on the page.
+
 	},
 
 	initialize: function(){
