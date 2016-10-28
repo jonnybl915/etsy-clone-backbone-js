b34add2cca770e68fa6f6fc7222b3c3a98483500
jonathandavidblack <jonnybl915@gmail.com>
2016-10-28 11:49:43 -0400

    added hash change for routing to particular objects

--- src/scripts/views-multi.js
+++ src/scripts/views-multi.js
@@ -3,6 +3,16 @@ const Backbone = require('backbone');
 const PlaceHolderEtsyView = Backbone.View.extend({
   el: '#app-container',
 
+  events: {
+    //type of event and the corresponding route
+    "click .etsy-card" : 'routeToIndividual'
+  },
+
+  routeToIndividual: function(evt){
+    console.log(evt.currentTarget.id);
+    //****** Changes The Hash Based On the Click ***********//
+    window.location.hash = evt.currentTarget.id;
+  },
 
 //=========================
 // BUILDING BLOCKS FOR HTML
@@ -11,8 +21,12 @@ const PlaceHolderEtsyView = Backbone.View.extend({
   _builderHTMLTemplate: function(collData){
     let htmlBuilderString = collData.models.map(function(model){
       return //return some thing via ${model.get(**variablename)}
+      //NEED TO GRAB THE UNIQUE ID HERE FOR ROUTING TO PARTICULAR OBJECTS
+      // something like<div class="etsy-card" id="model.get(lalala_id)"></div>
+      //here I can link up and change the hash to hit my different routes
+      //without an anchor tag. we can do this employing Backbone events
     }).join('')
-    return htmlBuilderString;
+    return htmlBuilderString; 
   },
 
   render: function(collectionData){
