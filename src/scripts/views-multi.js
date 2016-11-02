d9727226be6313f697bf87b18b8a8a576723d178
jonathandavidblack <jonnybl915@gmail.com>
2016-11-02 16:55:23 -0400

    working on carousel

--- src/scripts/views-multi.js
+++ src/scripts/views-multi.js
@@ -101,13 +101,3 @@ const MultipleListingView = Backbone.View.extend({
 })
 
 module.exports = MultipleListingView;
-
-(document).ready(function(){
-     $('.carousel').carousel({
-           dist:0,
-           shift:0,
-           padding:20,
-
-     });
-
-   });
